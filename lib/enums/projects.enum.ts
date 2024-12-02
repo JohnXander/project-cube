5e34d867d2453fd3e673d0402e42ba40dce947c3
John Bloxam <johnblox5@gmail.com>
2024-12-02 00:03:10 +0000

    use enums for project names

---
+++ lib/enums/projects.enum.ts
@@ -0,0 +1,8 @@
+export enum Project {
+  klipboar = "klipboar",
+  minilrs = "minilrs",
+  geoquix = "geoquix",
+  projectcube = "projectcube",
+  captivote = "captivote",
+  avatarify = "avatarify"
+}
---
+++ lib/enums/tech.enum.ts
@@ -0,0 +1,17 @@
+export enum Tech {
+  Electron = "Electron",
+  TypeScript = "TypeScript",
+  MongoDB = "MongoDB",
+  Express = "Express",
+  React = "React",
+  TanStackQuery = "TanStack Query",
+  Node = "Node",
+  Tailwind = "Tailwind",
+  Redux = "Redux",
+  Prisma = "Prisma",
+  tRPC = "tRPC",
+  Postgres = "Postgres",
+  NextJS = "Next JS",
+  ThreeJS = "Three.js",
+  MUI = "MUI"
+}
